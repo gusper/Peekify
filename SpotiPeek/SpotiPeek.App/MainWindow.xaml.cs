94358dfde3e68be99b255832d4cd6479bc585787
Gus Perez (Home) <gus@gusperez.com>
2015-01-29 18:59:58 -0800

    Quick first version to get things working in a very basic way.

---
+++ SpotiPeek/SpotiPeek.App/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SpotiPeek.App
+{
+    public partial class MainWindow : Window
+    {
+        private SpotifyManager _sm = new SpotifyManager();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            HookUpEventHandlers();
+        }
+
+        private void HookUpEventHandlers()
+        {
+            this.MouseLeftButtonDown += delegate { DragMove(); };
+            TrackInfoLabel.MouseUp += TrackInfoLabel_MouseUp;
+        }
+
+        private void TrackInfoLabel_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            RefreshContent();
+        }
+
+        private void RefreshContent()
+        {
+            TrackInfoLabel.Content = _sm.CurrentTrackInfo;
+        }
+
+        protected override void OnInitialized(EventArgs e)
+        {
+            base.OnInitialized(e);
+            RefreshContent();
+        }
+    }
+}
---
+++ SpotiPeek/SpotiPeek.App/SpotifyManager.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using SpotifyAPI.SpotifyLocalAPI;
+
+namespace SpotiPeek.App
+{
+    class SpotifyManager
+    {
+        private SpotifyLocalAPIClass _sApi;
+        private SpotifyEventHandler _sEvents;
+        private SpotifyMusicHandler _sMusic;
+
+        public SpotifyManager()
+        {
+            _sApi = new SpotifyLocalAPIClass();            
+            
+            if (!SpotifyLocalAPIClass.IsSpotifyRunning())
+            {
+                _sApi.RunSpotify();
+                Thread.Sleep(4000);
+            }
+
+            if (!SpotifyLocalAPIClass.IsSpotifyWebHelperRunning())
+            {
+                _sApi.RunSpotifyWebHelper();
+                Thread.Sleep(4000);
+            }
+
+            if (!_sApi.Connect())
+            {
+                MessageBox.Show("Failed to connect to Spotify.", "SpotiPeek", MessageBoxButton.OK);
+                Application.Current.Shutdown(-1);
+            }
+
+            _sMusic = _sApi.GetMusicHandler();
+            _sEvents = _sApi.GetEventHandler();
+        }
+
+        public string CurrentTrackInfo
+        {
+            get
+            {
+                _sApi.Update();
+                return string.Format("{0} by {1}", _sMusic.GetCurrentTrack().GetTrackName(), _sMusic.GetCurrentTrack().GetArtistName());
+            }
+        }
+    }
+}
