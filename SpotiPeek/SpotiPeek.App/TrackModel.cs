26d5cbcef919d8eb923aba157250a2566dde0174
Gus Perez (home) <gus@gusperez.com>
2016-09-26 20:26:47 -0700

    Added model for passing around info on tracks

---
+++ SpotiPeek/SpotiPeek.App/TrackModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpotiPeek.App
+{
+    class TrackModel
+    {
+        public string SongTitle;
+        public string ArtistName;
+        public string AlbumTitle;
+        public string AlbumYear;
+    }
+}
